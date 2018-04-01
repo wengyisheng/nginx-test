d4bf3c3741d9df48622a0581697cc7f47f0fb916
Your Name <you@example.com>
2018-04-02 00:12:37 +0800

    change

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+filepath=$(cd "$(dirname "$0")"; pwd)
 host_ip=`ip addr show dev eth0|egrep 'inet '|egrep -o '([0-9]{1,}\.){3}[0-9]{1,}'|awk -F '' 'NR==1{print}'`
 if [[ ${host_ip}x == x ]];then
 	host_ip=`ip addr show dev ens160|egrep 'inet '|egrep -o '([0-9]{1,}\.){3}[0-9]{1,}'|awk -F '' 'NR==1{print}'`
@@ -8,24 +9,13 @@ fi
 #update yum repodata
 sudo yum makecache fast
 
-#remove docker
-#sudo yum remove -y docker \
-#                   docker-client \
-#                   docker-client-latest \
-#                   docker-common \
-#                   docker-latest \
-#                   docker-latest-logrotate \
-#                   docker-logrotate \
-#                   docker-selinux \
-#                   docker-engine-selinux \
-#                   docker-engine
-
 #install required packages
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2 \
   wget \
-  ansible
+  ansible \
+  git
 
 #config yum resource
 sudo yum-config-manager -y \
@@ -71,61 +61,9 @@ fi
 #install jenkins cli
 cd ${JENKINS_HOME} && sudo wget http://${host_ip}:8899/jnlpJars/jenkins-cli.jar
 
-#create job config.xml
-cat << EOF > /${JENKINS_HOME}/test_job_config.xml
-<?xml version='1.0' encoding='UTF-8'?>
-<project>
-  <actions/>
-  <description></description>
-  <keepDependencies>false</keepDependencies>
-  <properties>
-    <hudson.model.ParametersDefinitionProperty>
-      <parameterDefinitions>
-        <hudson.model.ChoiceParameterDefinition>
-          <name>env</name>
-          <description></description>
-          <choices class="java.util.Arrays$ArrayList">
-            <a class="string-array">
-              <string>dev</string>
-              <string>prod</string>
-            </a>
-          </choices>
-        </hudson.model.ChoiceParameterDefinition>
-      </parameterDefinitions>
-    </hudson.model.ParametersDefinitionProperty>
-  </properties>
-  <scm class="hudson.plugins.git.GitSCM" plugin="git@3.8.0">
-    <configVersion>2</configVersion>
-    <userRemoteConfigs>
-      <hudson.plugins.git.UserRemoteConfig>
-        <url>https://github.com/xcl001987/nginx-test.git</url>
-      </hudson.plugins.git.UserRemoteConfig>
-    </userRemoteConfigs>
-    <branches>
-      <hudson.plugins.git.BranchSpec>
-        <name>*/master</name>
-      </hudson.plugins.git.BranchSpec>
-    </branches>
-    <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
-    <submoduleCfg class="list"/>
-    <extensions/>
-  </scm>
-  <canRoam>true</canRoam>
-  <disabled>false</disabled>
-  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
-  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
-  <triggers/>
-  <concurrentBuild>false</concurrentBuild>
-  <builders>
-    <hudson.tasks.Shell>
-      <command>ssh -o stricthostkeychecking=no root@10.82.12.62 &quot;cd /jenkins_home/workspace/${JOB_NAME} &amp;&amp; docker build -t nginx:${BUILD_NUMBER} .&quot;
-ssh -o stricthostkeychecking=no root@10.82.12.62 &quot;ansible-playbook -i ~/ansible_hosts.txt -e host_group=${env} -e version=${BUILD_NUMBER} ~/test.yml&quot;</command>
-    </hudson.tasks.Shell>
-  </builders>
-  <publishers/>
-  <buildWrappers/>
-</project>
-EOF
+git clone https://github.com/xcl001987/nginx-test.git
+cp ${filepath}/nginx-test/test_job_config.xml /${JENKINS_HOME}
+cp ${filepath}/nginx-test/test.yml /${JENKINS_HOME}
 sed -i "s/10.82.12.62/${host_ip}/" /${JENKINS_HOME}/test_job_config.xml
 
 #create job
@@ -139,21 +77,9 @@ sudo mkdir -p ~/.ssh && sudo chmod 700 ~/.ssh
 sudo chmod 600 ~/.ssh/authorized_keys
 
 #create ansible inventory file
-cat << EOF > ~/ansible_hosts.txt
+cat << EOF > /jenkins_home/ansible_hosts.txt
 [dev]
 127.0.0.1
 [prod]
 127.0.0.1
 EOF
-
-#create ansible playbook file
-cat << EOF > ~/test.yml
-- hosts: '{{host_group}}'
-  tasks:
-    - name: 'stop docker'
-      shell: "docker_id=`docker ps --filter name='nginx-test' -q` && (docker stop ${docker_id} || exit 0) && sleep 10" 
-    - name: 'run docker'
-      shell: 'docker run -d --name=nginx-test -p 9999:80 nginx:{{version}}'
-...
-EOF
-
